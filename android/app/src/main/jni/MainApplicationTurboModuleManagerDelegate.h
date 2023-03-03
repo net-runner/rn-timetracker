71d19bce10b46cce459b1655795c2b67642374d9
net-runner <slazynski.j@gmail.com>
2023-03-03 23:57:31 +0100

    Name change pt 2.

--- android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
+++ android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
@@ -14,7 +14,7 @@ class MainApplicationTurboModuleManagerDelegate
  public:
   // Adapt it to the package you used for your Java class.
   static constexpr auto kJavaDescriptor =
-      "Lcom/templatemobile/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
+      "Lcom/rn-timetracker/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
 
   static jni::local_ref<jhybriddata> initHybrid(jni::alias_ref<jhybridobject>);
 
